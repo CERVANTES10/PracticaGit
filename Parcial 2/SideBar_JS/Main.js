5daa7127c66da199491cc173503f05792dd19ca0
CERVANTES10 <andrescervantes190@gmail.com>
2023-10-16 22:04:06 -0500

    Gato2

--- Parcial 2/SideBar_JS/Main.js
+++ Parcial 2/SideBar_JS/Main.js
@@ -1,5 +1,7 @@
-document.getElementById("btn").addEventListener("click", function(){
+window.onload=function(){
+    document.getElementById("btn").addEventListener("click", function(){
         console.log("Click en el boton");
         document.getElementById("SideBar").classList.toggle("sideBarEscondido");
         document.getElementById("Content").classList.toggle("contentAlCien");
     })
+}
